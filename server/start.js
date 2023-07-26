438a9c9c6f6db2c79f8ee912b22b8f44c39e2ece
Kasia <kasia@Kasias-MacBook-Pro.local>
2023-07-26 12:03:15 +0200

    Fetch data from backend

--- server/start.js
+++ server/start.js
@@ -1,17 +1,31 @@
 const path = require('path');
+const fs = require('fs');
 
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 const koaStatic = require('koa-static');
 const getPort = require('get-port');
 
 async function runServer() {
     const port = await getPort({ port: 3000 });
-
     const app = new Koa();
+
+    const router = Router();
+
+    router.get('/channels', function(ctx) {
+        const channelsJson = fs.readFileSync(path.join(__dirname, 'channels.json'));
+        const channels = JSON.parse(channelsJson);
+        ctx.body = channels;
+    });
+
     app.use(koaStatic(path.join(__dirname, '..', 'static')));
+    app.use(bodyParser()); 
+    app.use(router.routes());
     app.listen(port);
 
     console.log(`server started at http://localhost:${port}/`);
 }
+
 
 runServer().catch(console.error);
